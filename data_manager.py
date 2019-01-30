b22921dcd9d200daa1ed98c7a11ee26428a36385
Katalin Csortos <katalin.csortos@gmail.com>
2019-01-30 21:17:20 +0100

    add: added sql query for updating the card title

--- data_manager.py
+++ data_manager.py
@@ -1,20 +1,20 @@
-import connection
+from connection import connection_handler
 
 
-@connection.connection_handler
+@connection_handler
 def get_cards_for_board(cursor, board_id):
     cursor.execute("""
         SELECT cards.id, cards.title, cards.is_archived, statuses.id AS status
         FROM cards
         JOIN statuses ON cards.status_id = statuses.id
         WHERE cards.board_id = %(board_id)s
-        ORDER BY cards.order_num DESC;
+        ORDER BY cards.order_num ASC;
         """, {'board_id': board_id})
 
     return cursor.fetchall()
 
 
-@connection.connection_handler
+@connection_handler
 def get_boards_infos(cursor):
     cursor.execute("""
         SELECT id, title, user_id, is_public
@@ -24,10 +24,19 @@ def get_boards_infos(cursor):
     return cursor.fetchall()
 
 
-@connection.connection_handler
+@connection_handler
 def insert_new_card(cursor, new_card_data):
     cursor.execute("""
         INSERT INTO cards
         (title, board_id, status_id, order_num, is_archived)
         VALUES (%(title)s, %(board_id)s, %(status_id)s, %(order_num)s, %(is_archived)s)
         """, new_card_data)
+
+
+@connection_handler
+def save_new_card_title(cursor, new_card_title_data):
+    cursor.execute("""
+        UPDATE cards
+        SET title = %(title)s
+        WHERE id = %(id)s;
+        """, new_card_title_data)
