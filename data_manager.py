137bba17d3ea2262040603c50eab4f964fd10b24
Katalin Csortos <katalin.csortos@gmail.com>
2019-01-30 19:42:44 +0100

    feat: saving new card fix: cards data requested in arranged by order_id now

--- data_manager.py
+++ data_manager.py
@@ -4,13 +4,12 @@ import connection
 @connection.connection_handler
 def get_cards_for_board(cursor, board_id):
     cursor.execute("""
-                    SELECT cards.id, cards.title, cards.is_archived, statuses.id AS status
-                    FROM cards
-                    JOIN statuses ON cards.status_id = statuses.id
-                    WHERE cards.board_id = %(board_id)s
-                    ORDER BY cards.order_num ASC;
-                    """,
-                   {'board_id': board_id})
+        SELECT cards.id, cards.title, cards.is_archived, statuses.id AS status
+        FROM cards
+        JOIN statuses ON cards.status_id = statuses.id
+        WHERE cards.board_id = %(board_id)s
+        ORDER BY cards.order_num DESC;
+        """, {'board_id': board_id})
 
     return cursor.fetchall()
 
@@ -18,8 +17,17 @@ def get_cards_for_board(cursor, board_id):
 @connection.connection_handler
 def get_boards_infos(cursor):
     cursor.execute("""
-                    SELECT id, title, user_id, is_public
-                    FROM boards
-                    """)
+        SELECT id, title, user_id, is_public
+        FROM boards
+        """)
 
     return cursor.fetchall()
+
+
+@connection.connection_handler
+def insert_new_card(cursor, new_card_data):
+    cursor.execute("""
+        INSERT INTO cards
+        (title, board_id, status_id, order_num, is_archived)
+        VALUES (%(title)s, %(board_id)s, %(status_id)s, %(order_num)s, %(is_archived)s)
+        """, new_card_data)
